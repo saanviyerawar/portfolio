c49b7875a2b1476af0aa68a354fd97949475a2aa
Ce Min Pangastur <cm.pang25@gmail.com>
2025-01-28 16:36:15 +1100

    adjusted router basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React, { useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ModeContext } from './contexts/ModeProvider';
 import Home from './pages/home/Home';
 import MenuButton from './components/MenuButton';
-import "./styles/index.css"
+import "./styles/index.css";
 
 function App() {
   const { mode } = useContext(ModeContext);
+
   return (
     <div className={mode}>
-      <MenuButton />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Router basename="/portfolio">
+        <MenuButton />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

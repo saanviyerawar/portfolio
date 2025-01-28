a1dfdef2471bb1b50331362785e4ac8691cc4487
Ce Min Pangastur <cm.pang25@gmail.com>
2025-01-28 16:45:32 +1100

    fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ModeContext } from './contexts/ModeProvider';
 import Home from './pages/home/Home';
 import MenuButton from './components/MenuButton';
-import "./styles/index.css";
+import './styles/index.css';
 
 function App() {
   const { mode } = useContext(ModeContext);
 
   return (
     <div className={mode}>
-      <Router basename="/portfolio">
-        <MenuButton />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <MenuButton />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import { ModeProvider } from './contexts/ModeProvider'; // Import ModeProvider
+import { ModeProvider } from './contexts/ModeProvider';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import { ParallaxProvider } from "react-scroll-parallax";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ModeProvider>
-      <ParallaxProvider>
-        <BrowserRouter>
+        <BrowserRouter basename='/portfolio'>
           <App />
         </BrowserRouter>
-      </ParallaxProvider>
     </ModeProvider>
   </React.StrictMode>
 );

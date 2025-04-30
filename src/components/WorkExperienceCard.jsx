c19c0c839fd85171d465b24b4266f415540626da
z5425350 <z5425350@ad.unsw.edu.au>
2025-05-01 00:34:30 +1000

    updates to work experience

--- src/components/WorkExperienceCard.jsx
+++ src/components/WorkExperienceCard.jsx
@@ -11,32 +11,32 @@ function WorkExperienceCard({ workExperience, onMouseEnter, onMouseLeave, onClic
   const workExperienceInformation = workExperience.information;
 
   return (
-    <div
-      className={`flex flex-col md:flex-row gap-4 md:gap-10 items-center 
-        2xl:w-[550px] md:w-[300px] w-full h-auto md:h-[100px] p-4 md:p-5 rounded-lg 
-        transform sticky self-start shadow-2xl border transition-transform duration-300 
-        hover:shadow-[rgba(0,0,0,0.8)] hover:-translate-y-2 clickable ${selected ? 'border-4' : 'border-gray-300 border-opacity-70 border-2'}`
-      }
-      style={{
-        backgroundColor: workExperience.colour,
-        color: workExperience.textColour,
-        transform: `rotate(${workExperience.rotate})`,
-        top: topValue,
-        zIndex: selected ? 10 : 1,
-      }}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      onClick={onClick}
-    >
-      <img
-        className="h-[80px] md:h-[100%] aspect-auto"
-        src={`${process.env.PUBLIC_URL}/${workExperienceInformation.image}-Logo.png`}
-        alt={`${workExperienceInformation.company} logo`}
-      />
-      <p className="2xl:text-2xl xl:text-xl font-semibold text-center">
-        {workExperienceInformation.title}
-      </p>
-    </div>
+  <div
+    className={`flex flex-col md:flex-row gap-4 md:gap-10 items-center 
+      2xl:w-[650px] md:w-[400px] w-full h-auto md:h-[140px] p-6 md:p-8 rounded-lg 
+      transform sticky self-start shadow-2xl border transition-transform duration-300 
+      hover:shadow-[rgba(0,0,0,0.8)] hover:-translate-y-2 clickable ${selected ? 'border-4' : 'border-gray-300 border-opacity-70 border-2'}`
+    }
+    style={{
+      backgroundColor: workExperience.colour,
+      color: workExperience.textColour,
+      transform: `rotate(${workExperience.rotate})`,
+      top: topValue,
+      zIndex: selected ? 10 : 1,
+    }}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+    onClick={onClick}
+  >
+    <img
+      className="h-[120px] md:h-[160px] w-auto max-h-[120px] max-w-[160px] object-contain"
+      src={`${process.env.PUBLIC_URL}/${workExperienceInformation.image}-Logo.png`}
+      alt={`${workExperienceInformation.company} logo`}
+    />
+    <p className="2xl:text-3xl xl:text-2xl font-semibold text-center">
+      {workExperienceInformation.title}
+    </p>
+  </div>
   );
 }
 
